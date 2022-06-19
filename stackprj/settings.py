beffec9b032db96e25008e163201fb8847220ca2
JamesMusembi <james.musembi@student.moringaschool.com>
2022-06-19 14:59:57 +0300

    connecting database

--- stackprj/settings.py
+++ stackprj/settings.py
@@ -81,8 +81,10 @@ WSGI_APPLICATION = 'stackprj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'stackover',
+        'USER': 'moringa',
+        'PASSWORD': 'james',
     }
 }
 
