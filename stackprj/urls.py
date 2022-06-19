8146cbd26590e7f0db1a9a051b3e14039f473b73
JamesMusembi <james.musembi@student.moringaschool.com>
2022-06-19 18:34:05 +0300

    login and logout system

--- stackprj/urls.py
+++ stackprj/urls.py
@@ -14,15 +14,27 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from stackusers import views as user_view
 from django.contrib.auth import views as auth_view
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('stackbase.urls')),
-    
-    # Authentication system
-    path('register/', user_view.register, name='register'),
-    path('login/', auth_view.LoginView.as_view(template_name='stackusers/login.html'), name='login'),
-]
+
+    # Authentication System
+    path('register/', user_view.register, name="register"),
+    path('login/', auth_view.LoginView.as_view(template_name="stackusers/login.html"), name='login'),
+    path('logout/', auth_view.LogoutView.as_view(template_name="stackusers/logout.html"), name='logout'), 
+
+    # Profile system
+    # path('profile/', user_view.profile, name="profile"),
+    # path('profile/update/', user_view.profile_update, name="profile_update")
+]   
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
