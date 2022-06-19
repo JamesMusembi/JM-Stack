d3844a318832a14511c58ab74739d7729b95c998
JamesMusembi <james.musembi@student.moringaschool.com>
2022-06-19 18:50:26 +0300

    login and logout system

--- stackprj/settings.py
+++ stackprj/settings.py
@@ -143,3 +143,9 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'stackbase:home'
+
+LOGIN_URL = 'login'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
