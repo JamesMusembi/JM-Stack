62d6eeeec0991d84182eb4ab15a35801d7b1c564
JamesMusembi <james.musembi@student.moringaschool.com>
2022-06-20 01:21:27 +0300

    creating question class

--- stackbase/models.py
+++ stackbase/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.urls import reverse
+from django.utils import timezone
+from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
 
-# Create your models here.
+class Question(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=10000)
+    # content = models.TextField(null=True, blank=True)
+    content = RichTextField()
+    likes = models.ManyToManyField(User, related_name='question_post')
+    date_created = models.DateTimeField(default=timezone.now)   
+    
+    def __str__(self):
+        return f'{self.user.username} - Question'
+    
+    def get_absolute_url(self):
+        return reverse('stackbase:question-detail', kwargs={'pk':self.pk})
+    
+    def total_likes(self):
+        return self.likes.count()
